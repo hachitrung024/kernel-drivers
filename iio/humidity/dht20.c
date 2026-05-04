1fcdee9eed046f2df30ce2119f8ca079ba316734
Trung Ha <hachitrung024@gmail.com>
2026-05-04 21:44:35 +0700

    allocate IIO device and private data

--- iio/humidity/dht20.c
+++ iio/humidity/dht20.c
@@ -8,10 +8,45 @@
 #include <linux/i2c.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
+
+#include <linux/iio/iio.h>
+
+struct dht20_data {
+	struct i2c_client	*client;
+	struct mutex		lock;	/* serializes I2C transactions */
+};
+
+static const struct iio_info dht20_info = {};
+
+static const struct iio_chan_spec dht20_channels[] = {};
 
 static int dht20_probe(struct i2c_client *client)
 {
-	return 0;
+	struct device *dev = &client->dev;
+	struct iio_dev *indio_dev;
+	struct dht20_data *data;
+
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C))
+		return -EOPNOTSUPP;
+
+	indio_dev = devm_iio_device_alloc(dev, sizeof(*data));
+	if (!indio_dev)
+		return -ENOMEM;
+
+	data = iio_priv(indio_dev);
+	data->client = client;
+	mutex_init(&data->lock);
+
+	i2c_set_clientdata(client, indio_dev);
+
+	indio_dev->name = "dht20";
+	indio_dev->modes = INDIO_DIRECT_MODE;
+	indio_dev->info = &dht20_info;
+	indio_dev->channels = dht20_channels;
+	indio_dev->num_channels = ARRAY_SIZE(dht20_channels);
+
+	return devm_iio_device_register(dev, indio_dev);
 }
 
 static const struct i2c_device_id dht20_id[] = {
